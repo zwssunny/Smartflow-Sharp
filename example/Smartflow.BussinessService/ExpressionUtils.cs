71f3139fcc1dfc3d3c5b5152c76aec520b8c1f98
chengderen <38171858+chengderen@users.noreply.github.com>
2019-02-28 10:47:26 +0800

    Update ExpressionUtils.cs

--- example/Smartflow.BussinessService/ExpressionUtils.cs
+++ example/Smartflow.BussinessService/ExpressionUtils.cs
@@ -1,52 +1,137 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace Smartflow.BussinessService
 {
+    internal enum ConstantCharacter
+    {
+        None,
+        /// <summary>
+        /// 模糊
+        /// </summary>
+        Like,
+        /// <summary>
+        /// 等于=
+        /// </summary>
+        Equals,
+        /// <summary>
+        /// 左边表达式
+        /// </summary>
+        Left,
+        /// <summary>
+        /// 右边表达式
+        /// </summary>
+        Right
+    }
+
     public class ExpressionUtils
     {
-        public static void ParseExpressionToWhere(Expression expression, StringBuilder sb)
+        public static string ParseToWhere(Expression expression)
         {
-            BinaryExpression binaryExpression;
-            switch (expression.NodeType)
+            BinaryExpression body = (expression as BinaryExpression);
+            StringBuilder buildWhere = new StringBuilder();
+            ParseBinaryExpression(body, buildWhere);
+            return string.IsNullOrEmpty(buildWhere.ToString()) ?
+                   string.Empty :
+                   string.Format(" And {0} ", buildWhere.ToString());
+        }
+
+        private static void ParseBinaryExpression(BinaryExpression expr, StringBuilder builder)
+        {
+            if (expr != null)
             {
-                case ExpressionType.Equal:
-                    binaryExpression = expression as BinaryExpression;
-                    MemberExpression memberExpression = (binaryExpression.Left as MemberExpression);
-                    string propertyName = memberExpression.Member.Name;
-                    string rightValue = GetValue(binaryExpression.Right);
-                    sb.AppendFormat(" {0}='{1}' ", propertyName, rightValue);
-                    break;
-                case ExpressionType.OrElse:
-                    binaryExpression = expression as BinaryExpression;
-                    sb.Append("(");
-                    BinaryExpression OLeftExpression = (binaryExpression.Left as BinaryExpression);
-                    ParseExpressionToWhere(OLeftExpression, sb);
-                    sb.Append("OR");
-                    BinaryExpression ORigthExpression = (binaryExpression.Right as BinaryExpression);
-                    ParseExpressionToWhere(ORigthExpression, sb);
-                    sb.Append(")");
-                    break;
-                case ExpressionType.AndAlso:
-                    binaryExpression = expression as BinaryExpression;
-                    BinaryExpression ALeftExpression = (binaryExpression.Left as BinaryExpression);
-                    ParseExpressionToWhere(ALeftExpression, sb);
-                    sb.Append("AND");
-                    BinaryExpression ARigthExpression = (binaryExpression.Right as BinaryExpression);
-                    ParseExpressionToWhere(ARigthExpression, sb);
-                    break;
-                default:
-                    break;
+                string character = Operator(expr.NodeType);
+                if (character.ToLower() == "or")
+                {
+                    builder.AppendFormat("  ( ");
+                }
+                
+                if (expr.Left != null)
+                {
+                    ParseBinaryExpression(expr.Left as BinaryExpression, builder);
+                    ParseMethodCall(expr.Left, builder);
+                    ParseMember(expr.Left as MemberExpression, builder, ConstantCharacter.Left);
+                    ParseConstant(expr.Left as ConstantExpression, builder, ConstantCharacter.Equals);
+                }
+
+                builder.Append(character);
+
+                if (expr.Right != null)
+                {
+                    ParseBinaryExpression(expr.Right as BinaryExpression, builder);
+                    ParseMethodCall(expr.Right, builder);
+                    ParseMember(expr.Right as MemberExpression, builder, ConstantCharacter.Right);
+                    ParseConstant(expr.Right as ConstantExpression, builder, ConstantCharacter.Equals);
+                }
+
+                if (character.ToLower() == "or")
+                {
+                    builder.AppendFormat("  ) ");
+                }
             }
         }
 
-        private static string GetValue(Expression expression)
+        private static void ParseMethodCall(Expression expr, StringBuilder builder)
         {
-            string returnValue = string.Empty;
-            switch (expression.NodeType)
+            if (expr is MethodCallExpression)
+            {
+                MethodCallExpression method = expr as MethodCallExpression;
+                string methodName = method.Method.Name;
+                switch (methodName)
+                {
+                    case "Contains":
+                        ParseMember(method.Object, builder, ConstantCharacter.Left);
+                        builder.Append(" Like ");
+                        ParseConstant(Expression.Constant(Expression.Lambda(method.Arguments[0]).Compile().DynamicInvoke()), builder, ConstantCharacter.Like);
+                        break;
+                    case "ToString":
+                        ParseConstant(Expression.Constant(Expression.Lambda(method, null).Compile().DynamicInvoke()), builder, ConstantCharacter.Equals);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void ParseMember(Expression expr, StringBuilder builder, ConstantCharacter character)
+        {
+            if (expr is MemberExpression)
+            {
+                MemberExpression member = expr as MemberExpression;
+                if ((member.Member.MemberType == System.Reflection.MemberTypes.Field
+                    || member.Member.MemberType == System.Reflection.MemberTypes.Property)
+                    && character == ConstantCharacter.Right)
+                {
+                    string value = Expression.Lambda(member).Compile().DynamicInvoke().ToString();
+                    ParseConstant(Expression.Constant(value), builder, ConstantCharacter.Equals);
+                }
+                else
+                {
+                    builder.AppendFormat(" {0} ", member.Member.Name);
+                }
+            }
+        }
+
+        private static void ParseConstant(Expression expr, StringBuilder builder, ConstantCharacter character)
+        {
+            if (expr is ConstantExpression)
+            {
+                ConstantExpression constant = expr as ConstantExpression;
+                builder.AppendFormat("'{0}'",
+                    (character == ConstantCharacter.Like) ?
+                    "%" + constant.Value.ToString() + "%" :
+                    constant.Value.ToString());
+            }
+        }
+
+        private static string Operator(ExpressionType nodeType)
+        {
+            string character = string.Empty;
+            switch (nodeType)
             {
                 case ExpressionType.Add:
                     break;
@@ -58,7 +143,9 @@ namespace Smartflow.BussinessService
                     break;
                 case ExpressionType.And:
                     break;
+                case ExpressionType.Call:
                 case ExpressionType.AndAlso:
+                    character = " And ";
                     break;
                 case ExpressionType.AndAssign:
                     break;
@@ -70,14 +157,11 @@ namespace Smartflow.BussinessService
                     break;
                 case ExpressionType.Block:
                     break;
-
                 case ExpressionType.Coalesce:
                     break;
                 case ExpressionType.Conditional:
                     break;
                 case ExpressionType.Constant:
-                    ConstantExpression constantExpression = (expression as ConstantExpression);
-                    returnValue = constantExpression.Value.ToString();
                     break;
                 case ExpressionType.Convert:
                     break;
@@ -96,6 +180,7 @@ namespace Smartflow.BussinessService
                 case ExpressionType.Dynamic:
                     break;
                 case ExpressionType.Equal:
+                    character = "=";
                     break;
                 case ExpressionType.ExclusiveOr:
                     break;
@@ -135,9 +220,7 @@ namespace Smartflow.BussinessService
                     break;
                 case ExpressionType.Loop:
                     break;
-                case ExpressionType.Call:
                 case ExpressionType.MemberAccess:
-                    returnValue = Expression.Lambda(expression).Compile().DynamicInvoke().ToString();
                     break;
                 case ExpressionType.MemberInit:
                     break;
@@ -174,6 +257,7 @@ namespace Smartflow.BussinessService
                 case ExpressionType.OrAssign:
                     break;
                 case ExpressionType.OrElse:
+                    character = " Or ";
                     break;
                 case ExpressionType.Parameter:
                     break;
@@ -224,14 +308,7 @@ namespace Smartflow.BussinessService
                 default:
                     break;
             }
-            return returnValue;
-        }
-
-        public static string ParseToWhere(Expression expression)
-        {
-            StringBuilder whereBuilder = new StringBuilder();
-            ExpressionUtils.ParseExpressionToWhere(expression as BinaryExpression, whereBuilder);
-            return (!String.IsNullOrEmpty(whereBuilder.ToString())) ? "AND " + whereBuilder.ToString() : "";
+            return character;
         }
     }
 }
