ef74575118d4d4f37255a27dc1af84c7a6e08125
chengderen <38171858+chengderen@users.noreply.github.com>
2019-03-06 15:49:38 +0800

    Update DynamicRepository.cs

--- example/Smartflow.BussinessService/DynamicRepository.cs
+++ example/Smartflow.BussinessService/DynamicRepository.cs
@@ -1,4 +1,4 @@
-﻿using Smartflow.Form;
+using Smartflow.Form;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +9,37 @@ namespace Smartflow.BussinessService
 {
     public class DynamicRepository
     {
+
+        private static Dictionary<string, Type> _cache = new Dictionary<string, Type>();
+
         public static object GetInstance(string instanceID, FormRelationship relationship)
         {
-            //relationship.Name
-
-            //DblHelper.CreateConnection()
-
             string sql = string.Format(" select * from {0} where instanceID='{0}'",
                             relationship.Name,
                             instanceID);
 
-            dynamic data = DblHelper.CreateConnection().Query(sql);
+            Dictionary<string, Type> properties = new Dictionary<string, Type>();
             foreach (FormRelationship item in relationship.Items)
             {
-
+                Dictionary<string, Type> subProperties = new Dictionary<string, Type>();
+                if (!_cache.ContainsKey(item.Name))
+                {
+                    Type subType = TypeCreator.Creator(item.Name, subProperties);
+                    Type genericType = typeof(List<>);
+                    properties[item.Name] = genericType.MakeGenericType(subType);
+                }
             }
 
-            return null;
+            Type baseType = TypeCreator.Creator(relationship.Name, properties);
+            Object dataObject = DblHelper.CreateConnection().Query(baseType, sql);
+
+            foreach (FormRelationship item in relationship.Items)
+            {
+                sql = string.Format(" select * from {0} where instanceID='{0}'",item.Name,instanceID);
+                dataObject.SetValue(item.Name, DblHelper.CreateConnection().Query(_cache[item.Name], sql));
+            }
+
+            return dataObject;
         }
     }
 }
